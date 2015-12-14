062e8f05a7d927fc9a9488c26d25b0bab53e673e
gujunmin <3013366498@qq.com>
2015-12-14 20:25:42 +0800

    modify button img src

--- src/show.js
+++ src/show.js
@@ -92,7 +92,7 @@ function showBtn(pos) {
 
         var attr = [
             ['id', 'btn'],
-            ['src', 'https://raw.githubusercontent.com/Ovilia/handian-chrome-extension/master/res/handian32.png']
+            ['src', 'https://github.com/SPxiaomin/youdao-chrome-extension/blob/master/dest/handian32.png']
         ];
         setAttr(button, attr);
 
